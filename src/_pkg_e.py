946de33eccbb0011524809d31a599ac51622fe1f
alexey.lysiuk <alexey.lysiuk@gmail.com>
2017-10-16 14:37:33 +0300

    add enet library

---
+++ src/_pkg_e.py
@@ -0,0 +1,31 @@
+#
+#    Build Your Mac: Configurable build environment for macOS
+#    Copyright (C) 2017 Alexey Lysiuk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from command import Library
+import repository
+
+
+pkg = repository.add_package
+
+
+pkg(
+    name='enet',
+    source='http://enet.bespin.org/download/enet-1.3.13.tar.gz',
+    checksum='e36072021faa28731b08c15b1c3b5b91b911baf5f6abcc7fe4a6d425abada35c',
+    commands=Library()
+)
