c76788030a072130119c8e1dedf1b778af1aa2d8
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-03-17 17:14:51 +0200

    fix libtool name

--- src/_pkg_l.py
+++ src/_pkg_l.py
@@ -36,8 +36,8 @@ pkg(
     commands=Library()
 )
 pkg(
-    name='libTool',
-    source='https://ftp.gnu.org/gnu/libTool/libTool-2.4.6.tar.xz',
+    name='libtool',
+    source='https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz',
     checksum='7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f',
     commands=Tool()
 )
