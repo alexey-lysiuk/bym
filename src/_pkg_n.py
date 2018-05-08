cd9e8bf9f8de77c7c2abc6b4810c58bfc578a96d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-05-08 22:30:01 +0300

    add nasm 2.13.03

---
+++ src/_pkg_n.py
@@ -0,0 +1,31 @@
+#
+#    Build Your Mac: Configurable build environment for macOS
+#    Copyright (C) 2017 Alexey Lysiuk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from command import Tool
+import repository
+
+
+pkg = repository.add_package
+
+
+pkg(
+    name='nasm',
+    source='https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/nasm-2.13.03.tar.xz',
+    checksum='812ecfb0dcbc5bd409aaa8f61c7de94c5b8752a7b00c632883d15b2ed6452573',
+    commands=Tool()
+)
