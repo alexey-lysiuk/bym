0210b8da8370a6df41714da9a66ea0403fd8e3a6
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-03-24 10:35:29 +0200

    add readline 8.0

---
+++ src/_pkg_r.py
@@ -0,0 +1,31 @@
+#
+#    Build Your Mac: Configurable build environment for macOS
+#    Copyright (C) 2017 Alexey Lysiuk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from command import Library
+import repository
+
+
+pkg = repository.add_package
+
+
+pkg(
+    name='readline',
+    source='https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz',
+    checksum='e339f51971478d369f8a053a330a190781acb9864cf4c541060f12078948e461',
+    commands=Library()
+)
