069b51b82e4878eacb502b864ed9bea53dfd884c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-07-27 12:31:50 +0300

    update openssl to 1.1.1c

--- src/_pkg_o.py
+++ src/_pkg_o.py
@@ -48,8 +48,8 @@ pkg(
 )
 pkg(
     name='openssl',
-    source='https://www.openssl.org/source/openssl-1.1.1b.tar.gz',
-    checksum='5c557b023230413dfb0756f3137a13e6d726838ccd1430888ad15bfb2b43ea4b',
+    source='https://www.openssl.org/source/openssl-1.1.1c.tar.gz',
+    checksum='f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90',
     commands=(
         Command('./Configure', 'no-shared', '--prefix=' + configuration.install_path, 'darwin64-x86_64-cc'),
         Install()
