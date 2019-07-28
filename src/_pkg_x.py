102c418a2efc402ca6dc0ec8502327cf58081dc8
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-07-28 10:23:08 +0300

    add xdelta 3.1.0

--- src/_pkg_x.py
+++ src/_pkg_x.py
@@ -24,6 +24,13 @@ pkg = repository.add_package
 
 
 pkg(
+    name='xdelta',
+    source='https://github.com/jmacd/xdelta-gpl/releases/download/v3.1.0/xdelta3-3.1.0.tar.gz',
+    checksum='114543336ab6cee3764e3c03202701ef79d7e5e8e4863fe64811e4d9e61884dc',
+    dependencies='xz',
+    commands=Library()
+)
+pkg(
     name='xz',
     source='https://downloads.sourceforge.net/project/lzmautils/xz-5.2.4.tar.gz',
     checksum='b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145',
