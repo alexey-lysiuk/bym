e14392fe6f091459970ae3269a07f63f354eb536
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-09-17 22:09:01 +0300

    add sodium 1.0.18

--- src/_pkg_s.py
+++ src/_pkg_s.py
@@ -175,6 +175,12 @@ pkg(
     commands=Library()
 )
 pkg(
+    name='sodium',
+    source='https://github.com/jedisct1/libsodium/archive/1.0.18-RELEASE.tar.gz',
+    checksum='b7292dd1da67a049c8e78415cd498ec138d194cfdb302e716b08d26b80fecc10',
+    commands=Library()
+)
+pkg(
     name='speex',
     source='http://downloads.xiph.org/releases/speex/speex-1.2.0.tar.gz',
     checksum='eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094',
