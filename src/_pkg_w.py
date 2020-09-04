4d2dbbe36d798a09f07f678b7e87d3e391262f1c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-09-04 16:34:49 +0300

    update wxwidgets to 3.1.4

--- src/_pkg_w.py
+++ src/_pkg_w.py
@@ -32,8 +32,8 @@ pkg(
 )
 pkg(
     name='wxwidgets',
-    source='https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxWidgets-3.1.3.tar.bz2',
-    checksum='fffc1d34dac54ff7008df327907984b156c50cff5a2f36ee3da6052744ab554a',
+    source='https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.4/wxWidgets-3.1.4.tar.bz2',
+    checksum='3ca3a19a14b407d0cdda507a7930c2e84ae1c8e74f946e0144d2fa7d881f1a94',
     dependencies=('jpeg', 'png', 'tiff'),
     commands=CMakeInstall(
         '-DwxBUILD_SHARED=NO',
