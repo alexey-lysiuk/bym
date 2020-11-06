f926a05130124979d8e0574c99c3443ca7b00c7c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2020-11-06 10:34:13 +0200

    update libjpeg-turbo to 2.0.5

--- src/_pkg_j.py
+++ src/_pkg_j.py
@@ -31,8 +31,8 @@ pkg(
 )
 pkg(
     name='jpeg-turbo',
-    source='https://downloads.sourceforge.net/project/libjpeg-turbo/2.0.4/libjpeg-turbo-2.0.4.tar.gz',
-    checksum='33dd8547efd5543639e890efbf2ef52d5a21df81faf41bb940657af916a23406',
+    source='https://downloads.sourceforge.net/project/libjpeg-turbo/2.0.5/libjpeg-turbo-2.0.5.tar.gz',
+    checksum='16f8f6f2715b3a38ab562a84357c793dd56ae9899ce130563c72cd93d8357b5d',
     dependencies='nasm',
     commands=CMakeInstall('-DENABLE_SHARED=NO')
 )
